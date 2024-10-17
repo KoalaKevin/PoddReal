2361c37438ef122d6aa0990009e1b75105d1d098
adamdylen <152890131+adamdylen@users.noreply.github.com>
2024-10-17 17:54:06 +0200

    Kategori modell

---
+++ PoddReal/Models/Kategori.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Kategori
+    {
+        public String? Name { get; set; }
+
+        public Kategori() { }
+
+        public Kategori(String name)
+        {
+            Name = name;
+        }
+
+    }
+}
