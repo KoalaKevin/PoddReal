3a29e5bdad44b4735aadec583b428c62751c70b0
adamdylen <152890131+adamdylen@users.noreply.github.com>
2024-10-22 14:55:19 +0200

    push

--- PoddReal/WinFormsApp1/Form1.cs
+++ PoddReal/WinFormsApp1/Form1.cs
@@ -22,7 +22,7 @@ namespace WinFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             UppdateraDataGridView();
-            //UppdateraKategoriListBox();
+            UppdateraKategoriListBox();
 
         }
 
